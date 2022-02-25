e3a58996cf82325997c28e4bcccf3eca51a71c69
Quentin Lambotte <quentin.lambotte@hotmail.com>
2022-02-25 12:05:34 +0100

    parcel.py: module to get parcel infos

---
+++ src/parcel.py
@@ -0,0 +1,60 @@
+"""
+This module collects data from a parcel given an adress.
+"""
+from shapely.geometry import Point, Polygon
+from utilities import bbox_enlarged, square_envelope
+import requests
+import typing
+import json
+
+def get_info_parcel(
+        street: str,
+        commune: str,
+        postcode: int,
+        number: str
+) -> json:
+    url = "https://api.basisregisters.vlaanderen.be/v1/adresmatch?"
+    r = requests.get(url, params={
+        "gemeentenaam": commune,
+        "straatnaam": street,
+        "huisnummer": number,
+        "postcode": postcode
+    })
+    return r.json()
+
+def get_object_details(parcel):
+    objects = parcel['adresMatches'][0]['adresseerbareObjecten']
+    details = {
+        'id_adress': parcel['adresMatches'][0]['identificator']['objectId']
+    }
+    for obj in objects:
+        if obj['objectType'] == 'gebouweenheid':
+            intermediate = requests.get(obj['detail']).json()
+            geometry_url = intermediate['gebouw']['detail']
+            geometry = requests.get(geometry_url).json()['geometriePolygoon']
+            details[f"building_{intermediate['identificator']['objectId']}"] = Polygon(geometry['polygon']['coordinates'][0])
+        if obj['objectType'] == 'perceel':
+            capakey = obj['objectId'].replace('-', '/')
+            url = (
+                "https://geoservices.informatievlaanderen.be/"
+                f"capakey/api/v2/parcel/{capakey}/"
+                )
+            r = requests.get(url).json()
+            bbox_coordinates = json.loads(r['geometry']['boundingBox'])
+            details['bbox'] = Polygon(bbox_coordinates['coordinates'][0])
+    return details
+
+def get_map_parcel(parcel_details, frac=0.3):
+    bbox = parcel_details['bbox'].bounds
+    x_min, y_min, x_max, y_max = square_envelope(bbox_enlarged(bbox, frac=frac))
+    url = (
+        "https://geoservices.informatievlaanderen.be/raadpleegdiensten/"
+        "GRB-basiskaart/wms?service=WMS&request=GETMAP&WIDTH=1034"
+        "&HEIGHT=1024&LAYERS=GRB_BSK&FORMAT=image/geotiff"
+        f"&SRS=EPSG:31370&BBOX={x_min},{y_min},{x_max},{y_max}"
+    )
+    r = requests.get(url)
+    name = parcel_details['id_adress']
+    with open(f'./data/{name}.tiff', 'wb') as file:
+        file.write(r.content)
+    return f'./data/{name}.tiff'
