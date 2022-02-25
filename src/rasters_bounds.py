42cbbff9c93e09f5d4df8c8d7df337361458c5ba
Quentin Lambotte <quentin.lambotte@hotmail.com>
2022-02-25 12:04:46 +0100

    rasters_bounds.py: collect rasters bounds from shapefiles in data/Kbl

---
+++ src/rasters_bounds.py
@@ -0,0 +1,27 @@
+"""
+This module extracts the bounds of the DRM and DTM raster files from de
+Flemish region, found in
+ - (DSM) http://www.geopunt.be/download?container=dhm-vlaanderen-ii-dsm-raster-1m&title=Digitaal%20Hoogtemodel%20Vlaanderen%20II,%20DSM,%20raster,%201m
+ - (DTM) http://www.geopunt.be/download?container=dhm-vlaanderen-ii-dtm-raster-1m&title=Digitaal%20Hoogtemodel%20Vlaanderen%20II,%20DTM,%20raster,%201m
+
+The data is extracted from:  https://www.geopunt.be/download?container=kaartbladversnijding-ngi-numeriek&title=Kaartbladversnijdingen
+"""
+
+import geopandas as gp
+
+gf = gp.read_file(
+    "./data/Kbl/Kbl.shp"
+)
+
+squares = gf[["UIDN","CODE", "geometry"]]
+
+# for i in range(gf.shape[0]):
+#     squares.loc[i,
+#                 ['west_bound',
+#                  'south_bound',
+#                  'east_bound',
+#                  'north_bound']
+#                 ] = squares.loc[i,'geometry'].bounds
+
+if __name__ == '__main__':
+    squares.to_csv('./data/rasters_limits.csv')
